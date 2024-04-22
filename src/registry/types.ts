e5bfd14f0bfbdb98692bb94546f6793b82f6d869
Christoph Langer <christoph.langer100@gmail.com>
2024-04-22 23:57:54 +0200

    implement bais registry structure

---
+++ src/registry/types.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+const registryEntry = z.object({
+  name: z.string(),
+  title: z.string(),
+  description: z.string(),
+  dependencies: z.array(
+    z.object({
+      name: z.string(),
+      url: z.string(),
+      version: z.string().optional(),
+    })
+  ),
+});
+
+export type RegistryEntry = z.infer<typeof registryEntry>;
+
+export const registry = z.record(registryEntry);
+export type Registry = z.infer<typeof registry>;
