7c11fe22a8647360147b9c7c996f391129912aac
Christoph Langer <christoph.langer100@gmail.com>
2024-04-24 11:44:36 +0200

    update docs (introduction)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,8 +13,8 @@ const Footer = React.forwardRef<
   return (
     <footer ref={ref} {...rest} className={cn('', className)}>
       <div className="container relative flex flex-wrap items-center justify-center gap-4 py-8">
-        <div className="border-border absolute left-8 top-0 h-px w-[calc(100%-4rem)] border-t border-dashed"></div>
-        <p className="text-muted-foreground mt-0 text-sm">
+        <div className="absolute left-8 top-0 h-px w-[calc(100%-4rem)] border-t border-dashed border-border"></div>
+        <p className="text-sm text-muted-foreground">
           Inspired by{' '}
           <Link
             href={'https://ui.shadcn.com/'}
@@ -32,19 +32,19 @@ const Footer = React.forwardRef<
             uilabs.dev
           </Link>
         </p>
-        <CircleIcon className="fill-muted text-muted h-2 w-2" />
+        <CircleIcon className="h-2 w-2 fill-muted text-muted" />
         <Link
           href={siteConfig.repository.url}
           target="_blank"
-          className="text-muted-foreground text-sm font-medium transition-colors hover:text-lime-500"
+          className="text-sm font-medium text-muted-foreground transition-colors hover:text-lime-500"
         >
           Github
         </Link>
-        <CircleIcon className="fill-muted text-muted h-2 w-2 " />
+        <CircleIcon className="h-2 w-2 fill-muted text-muted " />
         <Link
           href={`${siteConfig.repository.url}/blob/master/LICENSE`}
           target="_blank"
-          className="text-muted-foreground text-sm font-medium transition-colors hover:text-lime-500"
+          className="text-sm font-medium text-muted-foreground transition-colors hover:text-lime-500"
         >
           License
         </Link>
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,32 @@
 import type { MDXComponents } from 'mdx/types';
+import { cn } from './lib/utils';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => (
+      <h1 className="text-4xl font-medium text-foreground">{children}</h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="mt-4 text-lg text-muted-foreground">{children}</h2>
+    ),
+    h3: ({ className, ...props }) => (
+      <h3
+        className={cn('mt-12 text-2xl font-medium text-foreground', className)}
+        {...props}
+      />
+    ),
+    p: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLParagraphElement>) => (
+      <p
+        className={cn(
+          'leading-7 text-muted-foreground [&:not(:first-child)]:mt-6',
+          className
+        )}
+        {...props}
+      />
+    ),
     ...components,
   };
 }
